50ddae5e052ba92b895c81978e12e55ab337e655
Sam Havens <sam@mosaicml.com>
2022-09-27 23:31:17 +0000

    fix docstring

--- composer/callbacks/sam_callback.py
+++ composer/callbacks/sam_callback.py
@@ -1,7 +1,7 @@
 # Copyright 2022 MosaicML Composer authors
 # SPDX-License-Identifier: Apache-2.0
 
-"""Monitor stuff rate during training."""
+"""print MLM predictions during training."""
 
 import torch
 from transformers import AutoTokenizer
